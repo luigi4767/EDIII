0d9cd4debc107be5d2d8a12a17301590221b6d53
Luis Abanto <luisenriqueaq@gmail.com>
2021-05-15 03:38:08 -0300

    freertos

---
+++ Proyecto EDIII+FreeRTOS/Proyecto EDIII+FreeRTOS/lpc_chip_43xx/Debug/inc/fpu_init.d
@@ -0,0 +1,21 @@
+inc/fpu_init.o inc/fpu_init.d: ../inc/fpu_init.c ../inc/sys_config.h \
+ ../inc/cmsis.h ../inc/lpc_types.h ../inc/cmsis_43xx.h ../inc/core_cm4.h \
+ C:\Users\luis_\Documents\MCUXpresso\Proyecto\ EDIII+FreeRTOS\Proyecto\ EDIII+FreeRTOS\lpc_chip_43xx\inc/core_cmInstr.h \
+ C:\Users\luis_\Documents\MCUXpresso\Proyecto\ EDIII+FreeRTOS\Proyecto\ EDIII+FreeRTOS\lpc_chip_43xx\inc/core_cmFunc.h \
+ C:\Users\luis_\Documents\MCUXpresso\Proyecto\ EDIII+FreeRTOS\Proyecto\ EDIII+FreeRTOS\lpc_chip_43xx\inc/core_cm4_simd.h
+
+../inc/sys_config.h:
+
+../inc/cmsis.h:
+
+../inc/lpc_types.h:
+
+../inc/cmsis_43xx.h:
+
+../inc/core_cm4.h:
+
+C:\Users\luis_\Documents\MCUXpresso\Proyecto\ EDIII+FreeRTOS\Proyecto\ EDIII+FreeRTOS\lpc_chip_43xx\inc/core_cmInstr.h:
+
+C:\Users\luis_\Documents\MCUXpresso\Proyecto\ EDIII+FreeRTOS\Proyecto\ EDIII+FreeRTOS\lpc_chip_43xx\inc/core_cmFunc.h:
+
+C:\Users\luis_\Documents\MCUXpresso\Proyecto\ EDIII+FreeRTOS\Proyecto\ EDIII+FreeRTOS\lpc_chip_43xx\inc/core_cm4_simd.h:
---
+++ Proyecto EDIII+FreeRTOS/Proyecto EDIII+FreeRTOS/lpc_chip_43xx/Debug/src/ring_buffer.d
@@ -0,0 +1,7 @@
+src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
+ C:\Users\luis_\Documents\MCUXpresso\Proyecto\ EDIII+FreeRTOS\Proyecto\ EDIII+FreeRTOS\lpc_chip_43xx\inc/ring_buffer.h \
+ C:\Users\luis_\Documents\MCUXpresso\Proyecto\ EDIII+FreeRTOS\Proyecto\ EDIII+FreeRTOS\lpc_chip_43xx\inc/lpc_types.h
+
+C:\Users\luis_\Documents\MCUXpresso\Proyecto\ EDIII+FreeRTOS\Proyecto\ EDIII+FreeRTOS\lpc_chip_43xx\inc/ring_buffer.h:
+
+C:\Users\luis_\Documents\MCUXpresso\Proyecto\ EDIII+FreeRTOS\Proyecto\ EDIII+FreeRTOS\lpc_chip_43xx\inc/lpc_types.h:
