84869b1847b1a26abfb1a49416d8c3e7f6237990
Luis Abanto <luisenriqueaq@gmail.com>
2021-06-04 08:32:30 -0300

    freertos_vfinal

--- Proyecto EDIII+FreeRTOS/Proyecto EDIII+FreeRTOS/freertos_proyecto_ed3_queue/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lCMSIS_DSPLIB_CM4 -llpc_board_nxp_lpcxpresso_4337 -llpc_chip_43xx
-
